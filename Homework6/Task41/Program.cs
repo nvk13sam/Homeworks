6f8bd769768f0004e60753afd31c1cdfa7394a0f
nvk13sam <nvk13@yandex.ru>
2022-11-22 20:10:01 +0500

    add Homework6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.  
+// 0, 7, 8, -2, -2 -> 2
+
+// // V 1.0
+// //Преобразуем строку в массив строк.
+
+// string[] stringToArray(string s)
+// {
+//     string[] stringArray = s.Split(",");
+//     return stringArray;
+// }
+
+// //Преобразуем массив сторок в массив целых чисел.
+
+// int[] strToIntArray(string[] stringArray)
+// {
+//     int[] intArray = new int[stringArray.Length];
+//     for (int i = 0; i < intArray.Length; i++)
+//     {
+//         intArray[i] = Convert.ToInt32(stringArray[i]);
+//     }
+//     return intArray;
+// }
+
+// //Считаем количество положительных чисел.
+
+// int positiveCount(int[] intArray)
+// {
+//     int count = 0;
+//     for (int i = 0; i < intArray.Length; i++)
+//     {
+//         if (intArray[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// //Решение.
+
+// Console.Write("Введите числа через запятую: ");
+// string str = Console.ReadLine();
+// string[] strArray = stringToArray(str);
+// int[] intArray = strToIntArray(strArray);
+// Console.WriteLine($"Положительных чисел в массиве " + positiveCount(intArray));
+
+
+//************************************************
+// V 2.0
+Console.WriteLine("Введите числа через запятую: ");
+string str = Console.ReadLine();
+
+var ArrayString = str.Split(",");
+int[] arr = Array.ConvertAll(ArrayString, int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+	if (arr[i] > 0) count++;
+}
+Console.WriteLine($"Положительных элементов в данном массиве {count}");
+
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// // V 1.0
+// // Считаем координату x точки пересечения.
+
+// double calculateX(double b1, double k1, double b2, double k2)
+// {
+//     return (b2 - b1) / (k1 - k2);
+// }
+
+// // Считаем координату y точки пересечения.
+
+// double calculateY(double b1, double k1, double x)
+// {
+//     return k1 * x + b1;
+// }
+
+// // Решение.
+
+// Console.Write("Введите коэффициент b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Введите коэффициент k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Введите коэффициент b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Введите коэффициент k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = calculateX(b1, k1, b2, k2);
+// double y = calculateY(b1, k1, x);
+// Console.WriteLine($"({x};{y})");
+
+//***********************************************
+// V 2.0
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+ 
+var x = (b2 - b1)/(k1 - k2);
+var y = k1 * x + b1;
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
